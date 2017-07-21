9add8f10b9e72ef0420b383b22b5103a8c6a69ff
Christian Tietze <christian.tietze@gmail.com>
2017-07-21 08:32:53 +0200

    add macOS app target

---
+++ Example macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Example macOS
+//
+//  Created by Christian Tietze on 21.07.17.
+//  Copyright © 2017 Rudd Fawcett. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Example macOS
+//
+//  Created by Christian Tietze on 21.07.17.
+//  Copyright © 2017 Rudd Fawcett. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
